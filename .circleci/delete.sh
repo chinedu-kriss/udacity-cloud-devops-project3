88dc7272a3748c5b502ac99ee05d93c12ce0318c
Chinedu Kriss Gbulie <63459638+chinedu-kriss@users.noreply.github.com>
2022-07-13 12:23:19 +0100

    Configure Infrastructure using Ansible Playbook

---
+++ .circleci/delete.sh
@@ -0,0 +1,3 @@
+aws s3 rm s3://udapeople-3714207 --recursive
+aws cloudformation delete-stack --stack-name udapeople-backend-3714207
+aws cloudformation delete-stack --stack-name udapeople-frontend-3714207
